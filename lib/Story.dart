723afff2fb523acbefacd2e6f52364eca7d96bd8
IrinaSemGB <irina.semyachkina@mail.ru>
2021-07-16 07:55:44 +0300

    Step 2,3,4 - Create a new class called Story

---
+++ lib/Story.dart
@@ -0,0 +1,12 @@
+class Story {
+
+  String storyTitle = '';
+  String choice1 = '';
+  String choice2 = '';
+
+  Story({title, ch1, ch2}) {
+    title = storyTitle;
+    ch1 = choice1;
+    ch2 = choice2;
+  }
+}
